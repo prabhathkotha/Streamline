f6c29de0272983ae6e766005149c25a31aa7f223
Jacob <littlej3800@gmail.com>
2019-11-14 16:55:18 -0500

    Recommendations are now live.

--- report_card/recommend_platform.js
+++ report_card/recommend_platform.js
@@ -1,6 +1,64 @@
-const recommendPlatform = function(type, duration, popularity, decade, rating) {
+
+const recommendPlatform = async function(type, duration, popularity, decade, rating) {
+    /** type:movie v. show
+     *      0 to 100
+     *  - duration: show length
+     *      0 to 100 
+     *  - popularity:
+     *      blockbuster v niche
+     *  - decade: 
+     *      0 to 100
+     *  - rating:
+     *      child, parental, adult
+     * 
+    */
     //TODO
     // do some magic here
     // return streaming provider and three imdb ids to display
         // { platform: 'netflix', suggestions: ['imdbid_a', 'imdbid_b', 'imdbid_c'] }
+        // movie vs show:
+        // show duration preference:
+        // popularity preference (movie or show importance)
+        // age of movies:
+        // ratings:
+    /** request report_cards */
+    let report_card = await $.get('../report_card/reportCards.json', (data) => {}, 'json');
+    let score = {};
+    let maxShow = 0;
+    let maxMovie = 0;
+    for(let i in report_card) {
+        if( report_card[i].movies.avgPopularity > maxMovie)
+            maxMovie = report_card[i].movies.avgPopularity;
+        if(report_card[i].shows.avgPopularity > maxShow)
+            maxShow = report_card[i].shows.avgPopularity;    
+    }
+    for(let i in report_card) {
+        let provider = i;
+        // let provider = report_card[i];
+        // console.log(i);
+        score[provider] = 0;
+        score[provider] += 0.2 * ((report_card[i].movies.avgPopularity / maxMovie) * (1 - type/100) 
+                                + (report_card[i].shows.avgPopularity / maxShow) * (type/100));
+        // runtime needs to be broken into buckets. for now, they are similar so all get full marks
+        score[provider] += 0.2
+        // ratio of movies within past year
+        let newMovies = report_card[i].movies.yearDistr['2019'] / report_card[i].movies.count;
+        let newShows = report_card[i].shows.yearDistr['2019'] / report_card[i].shows.count;
+        newMovies = newMovies != undefined ? newMovies : 0; // in case a service has 0 new content
+        newShows = newShows != undefined ? newShows : 0; // in case a service has 0 new movies
+        let weightedNewness = (newMovies * (1 - type/100)) + (newShows * (type/100));
+        score[provider] += 0.2 * ( 1 - Math.abs(weightedNewness  - (decade / 100) )); // percentage closeness to newness preference
+
+    }
+    let match = '';
+    let matchScore = 0;
+    console.log("Scorecard: ")
+    console.log(score);
+    for(let x in score) {
+        if (score[x] > matchScore) {
+            match = x;
+            matchScore = score[x];
+        }
+    }
+    return {'platform': match, suggestions: []};
 };
--- streamline/js/index.js
+++ streamline/js/index.js
@@ -15,32 +15,34 @@ $("#child, #parental, #adult").click(function() {
     rating = $(this).attr("id");
 });
 
-$("#done").click(function() {
-    let recommendation = recommendPlatform(
-        $("#type").val(),
-        $("#duration").val(),
-        popularity,
-        $("#decade").val(),
-        rating
-    );
+$("#done").one('click', function(e) {
+    // future work: replace function call with http request         
+    const user_input = {
+        type: $("#type").val(),
+        duration: $("#duration").val(),
+        popularity: popularity,
+        decade: $("#decade").val(),
+        rating: rating
+    };
+    recommendPlatform(user_input).then(recommendation => {
+        //debug ~~~
+        // recommendation = {platform: 'Netflix', suggestions: ['tt0110912', 'tt4633694', 'tt9243946']};
+        
+        //display recommended streaming platform
+        $('.suggestion-platform').text(recommendation.platform);
 
-    //debug ~~~
-    recommendation = {platform: 'Netflix', suggestions: ['tt0110912', 'tt4633694', 'tt9243946']};
-    
-    //display recommended streaming platform
-    $('.suggestion-platform').text(recommendation.platform);
-
-    //display movies based on imdb ids in recommendation
-    recommendation.suggestions.forEach(function(imdb_id, i) {
-        $.get(`http://www.omdbapi.com/?apikey=c6f70b0d&i=${imdb_id}`, function(data) {
-            $(`#suggestion-${i+1}-pic`).attr("src", data.Poster);
-            $(`#suggestion-${i+1}-desc`).text(data.Plot);
+        //display movies based on imdb ids in recommendation
+        recommendation.suggestions.forEach(function(imdb_id, i) {
+            $.get(`http://www.omdbapi.com/?apikey=c6f70b0d&i=${imdb_id}`, function(data) {
+                $(`#suggestion-${i+1}-pic`).attr("src", data.Poster);
+                $(`#suggestion-${i+1}-desc`).text(data.Plot);
+            });
         });
-    });
 
-    //display and scroll
-    $("#suggestions").css("display", "block");
-    $('html,body').animate({
-        scrollTop: $("#suggestions").offset().top
-    },'slow');
+        //display and scroll
+        $("#suggestions").css("display", "block");
+        $('html,body').animate({
+            scrollTop: $("#suggestions").offset().top
+        },'slow');
+    });
 });
